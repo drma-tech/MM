ac8fb30482afe7a420067a6e27898915619c20c2
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-12-06 17:32:36 +0700

    is printscreen

--- MM.WEB/wwwroot/js/main.js
+++ MM.WEB/wwwroot/js/main.js
@@ -5,6 +5,7 @@
 export const isLocalhost = location.host.includes("localhost");
 export const isDev = location.hostname.includes("dev.");
 export const isWebview = /webtonative/i.test(navigator.userAgent);
+export const isPrintScreen = location.href.includes("printscreen");
 
 export const servicesConfig = {
     AnalyticsCode: "G-P7B5BSBS9S",
