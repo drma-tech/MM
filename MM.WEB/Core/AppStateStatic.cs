5072c32454e9a91a50402f25bfba61ad111237db
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-12-06 21:13:05 +0700

    ...

--- MM.WEB/Core/AppStateStatic.cs
+++ MM.WEB/Core/AppStateStatic.cs
@@ -39,12 +39,6 @@ public static class AppStateStatic
 
             var cache = await js.Utils().GetLocalStorage("platform");
 
-            if (cache.Empty() && js != null) //shouldn't happen (because it's called in index.html)
-            {
-                await js.InvokeVoidAsync("LoadAppVariables");
-                cache = await js.Utils().GetLocalStorage("platform");
-            }
-
             if (cache.NotEmpty())
             {
                 if (System.Enum.TryParse<Platform>(cache, true, out var platform) && System.Enum.IsDefined(platform))
