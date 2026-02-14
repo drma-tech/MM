3dab9224321104fd0bc39b1b833a9d636bcc95ce
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-02-14 14:02:30 -0300

    code sync

--- MM.WEB/Core/Helper/JavascriptHelper.cs
+++ MM.WEB/Core/Helper/JavascriptHelper.cs
@@ -204,7 +204,7 @@ namespace MM.WEB.Core.Helper
 
         public Task InitUserBack(string version) => InvokeVoid("services.initUserBack", version);
 
-        public Task InitAdSense(string adClient, GoogleAdSense.AdUnit adSlot, string? adFormat, string containerId) => InvokeVoid("services.initAdSense", adClient, ((long)adSlot).ToString(), adFormat, containerId);
+        public Task InitAdSense(string adClient, GoogleAdSense.AdUnit adSlot, string containerId) => InvokeVoid("services.initAdSense", adClient, ((long)adSlot).ToString(), containerId);
     }
 
     public class SwiperJs(IJSRuntime js) : JsModuleBase(js, "./js/swiper.js")
