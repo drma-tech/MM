4fb57a98c334baf985aa6b14a812146511f1972f
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-03-13 14:56:36 -0300

    Disallow: /_framework/ / new component seo meta and social meta

--- MM.WEB/Resources/SeoTranslations.Designer.cs
+++ MM.WEB/Resources/SeoTranslations.Designer.cs
@@ -19,7 +19,7 @@ namespace MM.WEB.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class SeoTranslations {
@@ -79,20 +79,20 @@ namespace MM.WEB.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Modern Matchmaker.
-        /// </summary>
-        public static string AppName {
-            get {
-                return ResourceManager.GetString("AppName", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Find your life partner.
         /// </summary>
         public static string AppSubtitle {
             get {
                 return ResourceManager.GetString("AppSubtitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Modern Matchmaker.
+        /// </summary>
+        public static string AppTitle {
+            get {
+                return ResourceManager.GetString("AppTitle", resourceCulture);
             }
         }
         
