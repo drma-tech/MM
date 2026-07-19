3ce6f4e24cf3d3f28495282e42137ea562eb3d58
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-07-19 18:04:06 -0300

    ...

--- MM.Shared/Enums/Language.cs
+++ MM.Shared/Enums/Language.cs
@@ -4,6 +4,9 @@
 ///     https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
 ///     https://en.wikipedia.org/wiki/List_of_official_languages
 ///     https://en.wikipedia.org/wiki/List_of_official_languages_by_country_and_territory
+///     
+///     todo: review and add missing languages
+///     https://www.loc.gov/standards/iso639-2/php/code_list.php
 /// </summary>
 public enum Language
 {
